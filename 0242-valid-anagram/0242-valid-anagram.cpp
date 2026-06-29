9ac5350c08e27a08a45ed05418428aa29494ca9b
Kunal Upadhyay <136359160+whokunalupadhyay@users.noreply.github.com>
2026-06-29 14:44:36 +0530

    Time: 4 ms (34.96%), Space: 10.1 MB (11.27%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> need;
+        unordered_map<char,int> want;
+
+        for(char ch: s){
+            need[ch]++;
+        }
+        for(char ch: t){
+            want[ch]++;
+        }
+
+        if(need == want){
+            return true;
+        }
+        return false;
+        
+    }
+};
