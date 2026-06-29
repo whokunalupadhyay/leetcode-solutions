9777b63588d678ba0706250ae52230c3d023bd39
Kunal Upadhyay <136359160+whokunalupadhyay@users.noreply.github.com>
2026-06-29 14:46:43 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
         unordered_map<char,int> need;
         unordered_map<char,int> want;
 
