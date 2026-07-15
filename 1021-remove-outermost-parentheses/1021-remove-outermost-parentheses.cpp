5f0ff0509f9670bb92106b9a9a3cf70b6407a52e
Kunal Upadhyay <136359160+whokunalupadhyay@users.noreply.github.com>
2026-07-15 15:10:04 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.97%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-
+        int i=0;
         string ans;
-        int i = 0;
         int depth = 0;
         while(i < s.length()){
             if(s[i] == '('){
                 if(depth > 0){
                     ans.push_back(s[i]);
-                    depth++;
                 }
-                else{
-                    depth++;
-                }
+                depth++;
             }
             else{
                 depth--;
                 if(depth > 0){
                     ans.push_back(s[i]);
                 }
+
             }
             i++;
         }
