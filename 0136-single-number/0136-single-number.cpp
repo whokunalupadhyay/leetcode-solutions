132866febe4e1d6c74b923df042707bf6a1e1574
Kunal Upadhyay <136359160+whokunalupadhyay@users.noreply.github.com>
2026-07-22 14:55:01 +0530

    Time: 0 ms (100%), Space: 20.5 MB (84.08%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> list;
-
-        for(int i: nums){
-            list[i]++;
-        }
-
-        int ans;
-
-        for(auto i: list){
-            if((i.second) == 1){
-                ans = i.first;
-            }
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            ans = ans^nums[i];
         }
 
         return ans;
-        
     }
 };
